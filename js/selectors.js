876e3a8bb7ecca869e902e06cb554c325597305b
Sonik S <gryazzz@gmail.com>
2017-12-25 19:06:27 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p').innerHTML;
+}
+
+function lastImageSelector() {
+  return $('li:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
